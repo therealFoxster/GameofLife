9d39706ce6aa6d6094031f41564f9fc49af2486c
edwinrlambert <edwinronaldlambert30@gmail.com>
2022-01-23 10:47:49 -0800

    Version 2: Forked from Huy's code.

--- src/Game.java
+++ src/Game.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Game {
     public static Scanner keyboard;
 
-    public static final void main(String[] args) {
+    public static void main(String[] args) {
         keyboard = new Scanner(System.in);
 
         instructions();
